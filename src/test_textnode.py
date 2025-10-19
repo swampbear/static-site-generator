59d4f33cb8ee496e25856aab2fb977ed80581f9f
Jobjorn <672641@stud.hvl.no>
2025-10-19 16:59:19 +0200

    feat: added tests for all text types related to text to html conversion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -29,6 +29,35 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_bold(self):
+        node = TextNode("This text is really bold", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This text is really bold")
+
+    def test_italic(self):
+        node = TextNode("This text is quite italic", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This text is quite italic")
+
+    def test_code(self):
+        node = TextNode("Code is so cool", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "Code is so cool")
+
+    def test_link(self):
+        node = TextNode("A link to the past", TextType.LINK)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "A link to the past")
+
+    def test_image(self):
+        node = TextNode("pretty cat", TextType.IMAGE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+
 
 if __name__ == "__main__":
     unittest.main()
