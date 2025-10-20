57d62415207cd29ddd236963f983c5c632ec2258
Jobjorn <672641@stud.hvl.no>
2025-10-20 14:58:22 +0200

    tests: tested italic, blod and code, start, end and multiple inlines

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -11,3 +11,36 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(
             TextNode("This is text with a ", TextType.TEXT, None), new_nodes[0]
         )
+
+    def test_italic(self):
+        node = TextNode("I love _italians_, I mean it", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(TextNode("italians", TextType.ITALIC, None), new_nodes[1])
+
+    def test_bold(self):
+        node = TextNode("That is pretty **bold** of you", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(TextNode("bold", TextType.BOLD, None), new_nodes[1])
+
+    def test_multiple_inline(self):
+        node = TextNode(
+            "This is text with a `code block``word`, and this is `another`",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            TextNode("This is text with a ", TextType.TEXT, None), new_nodes[0]
+        )
+        self.assertEqual(TextNode("code block", TextType.CODE, None), new_nodes[1])
+        self.assertEqual(TextNode("word", TextType.CODE, None), new_nodes[3])
+        self.assertEqual(TextNode("another", TextType.CODE, None), new_nodes[5])
+
+    def test_start_of_node(self):
+        node = TextNode("**bold** of you sir", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(TextNode("bold", TextType.BOLD, None), new_nodes[1])
+
+    def test_end_of_node(self):
+        node = TextNode("You are not _italian_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(TextNode("italian", TextType.ITALIC, None), new_nodes[1])
