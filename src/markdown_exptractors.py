b98345a8c5c8c7c31264229791176af66b1ba507
Jobjorn <672641@stud.hvl.no>
2025-10-23 21:51:07 +0200

    feat: added markdown image extractor and initial test

---
+++ src/markdown_exptractors.py
@@ -0,0 +1,11 @@
+import re
+
+# Regex for links (\w+:\/\/)(\w+).(\w+\.\w+\/)(\w+\.\w+) returns https://i.imgur.com/fJRm4Vk.jpeg
+
+## Regex for images (\[).+?(\]) returns ![words inside]
+
+
+def extract_markdown_images(text):
+    matches = re.findall(r"\!\[(.+?)\]\((.*?)\)", text)
+
+    return matches
---
+++ src/test_markdown_extractors.py
@@ -0,0 +1,17 @@
+import unittest
+
+from markdown_exptractors import extract_markdown_images
+
+
+class TestMarkdownExtractors(unittest.TestCase):
+
+    def test_exctract_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        extracted_images = extract_markdown_images(text)
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            extracted_images,
+        )
