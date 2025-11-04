d19d968223195747ea6e33bbd7cfb940bbff6914
Jobjorn <672641@stud.hvl.no>
2025-11-04 15:57:44 +0100

    feat: fixed bug in split link and initial test for link and imagetest for multiple texttype nodes

--- src/split_nodes_embed.py
+++ src/split_nodes_embed.py
@@ -6,21 +6,47 @@ from textnode import TextNode, TextType
 
 def split_nodes_image(old_nodes):
     new_nodes = []
+    found_image = False
     for node in old_nodes:
         if node.text_type is not TextType.TEXT:
             new_nodes.append(node)
             continue
         text = node.text
         matches = extract_markdown_images(text)
-        splt = re.split(r"\!\[.*?\]\(.*?\)", text)
-        for i in range(0, len(splt)):
-            if splt[i] != "":
-                new_nodes.append(TextNode(splt[i], TextType.TEXT))
-            if i < len(splt) - 1:
+        parts = re.split(r"\!\[.*?\]\(.*?\)", text)
+        if len(parts) > 1:
+            found_image = True
+        for i in range(0, len(parts)):
+            if parts[i] != "":
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+            if i < len(parts) - 1:
                 new_nodes.append(TextNode(matches[i][0], TextType.IMAGE, matches[i][1]))
+
+    if not found_image:
+        raise Exception("Error: no image found")
 
     return new_nodes
 
 
 def split_nodes_link(old_nodes):
-    pass
+    new_nodes = []
+    found_link = False
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        text = node.text
+        matches = extract_markdown_links(text)
+        parts = re.split(r"[^!]\[.*?\]+\(.*?\)", text)
+        print(parts)
+        if len(parts) > 1:
+            found_link = True
+        for i in range(0, len(parts)):
+            if parts[i] != "":
+                new_nodes.append(TextNode(parts[i] + " ", TextType.TEXT))
+            if i < len(parts) - 1:
+                new_nodes.append(TextNode(matches[i][0], TextType.LINK, matches[i][1]))
+    if not found_link:
+        raise Exception("Error: no link found")
+
+    return new_nodes
--- src/test_split_nodes_embed.py
+++ src/test_split_nodes_embed.py
@@ -5,6 +5,7 @@ from textnode import TextNode, TextType
 
 
 class TestSplitNodesEmbed(unittest.TestCase):
+    # ===Test Image Splits===
     def test_split_images(self):
         node = TextNode(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
@@ -18,6 +19,60 @@ class TestSplitNodesEmbed(unittest.TestCase):
                 TextNode(" and another ", TextType.TEXT),
                 TextNode(
                     "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_multiple_nodes_different_types_image(self):
+
+        nodes = [
+            TextNode(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+                TextType.TEXT,
+            ),
+            TextNode("bold text should stay as a bold node", TextType.BOLD),
+            TextNode(
+                " new images incomming and another ![second image](https://i.imgur.com/3elNhQu.png)",
+                TextType.TEXT,
+            ),
+        ]
+        new_nodes = split_nodes_image(nodes)
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode("bold text should stay as a bold node", TextType.BOLD),
+                TextNode(" new images incomming and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_no_images(self):
+        node = TextNode(
+            "This is text has no image [this is a link](youtube.com)",
+            TextType.TEXT,
+        )
+        with self.assertRaises(Exception):
+            _ = split_nodes_image([node])
+
+    # ===Test Link Splits===
+    def test_split_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
                 ),
             ],
             new_nodes,
