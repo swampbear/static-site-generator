72706a53f7f8afddcba0dce3076da125d086e0b5
Jobjorn <672641@stud.hvl.no>
2025-11-07 16:25:06 +0100

    test: test for no delimiter and no inkluded image or link.

--- src/test_text_to_textnodes.py
+++ src/test_text_to_textnodes.py
@@ -27,14 +27,35 @@ class TestTextToTextnodes(unittest.TestCase):
         )
 
     def test_no_delimiters(self):
-        text = "This is text with an italic word and a code block and an obiwan and a bootdev video"
+        text = "This is text with an italic word and a code block and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
         new_nodes = text_to_textnodes(text)
         self.assertListEqual(
             [
                 TextNode(
-                    "This is text with an italic word and a code block and an obiwan and a bootdev video",
+                    "This is text with an italic word and a code block and an ",
                     TextType.TEXT,
                 ),
+                TextNode(
+                    "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+                ),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            new_nodes,
+        )
+
+    def test_no_image_or_link(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an obi wan image and a link"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an obi wan image and a link", TextType.TEXT),
             ],
             new_nodes,
         )
