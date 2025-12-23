c86275b26f037955cb449e94a72ad1f67c892f1a
Jobjorn <672641@stud.hvl.no>
2025-12-23 18:46:29 +0100

    buildscript and changes to main and generate page

--- src/generate/generate_page.py
+++ src/generate/generate_page.py
@@ -3,7 +3,7 @@ import os
 from transformers.markdown_to_html_node import markdown_to_html_node
 from generate.extract_title import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -22,6 +22,8 @@ def generate_page(from_path, template_path, dest_path):
     new_content = template_content
     new_content = new_content.replace("{{ Title }}", title)
     new_content = new_content.replace("{{ Content }}", html)
+    new_content = new_content.replace('href="/', f'href="/{basepath}')
+    new_content = new_content.replace('src="/', f'src="/{basepath}')
 
     if os.path.isfile(dest_path):
         with open(dest_path, "w") as file:
@@ -29,3 +31,16 @@ def generate_page(from_path, template_path, dest_path):
     else:
         file = open(dest_path, "x")
         file.write(new_content)
+
+
+def generate_pages_recursive(source,template, dest, basepath):
+    content_dir = os.listdir(source)
+    for f in content_dir:
+        if os.path.isfile(f"{source}/{f}"):
+            if f[-3:] == ".md":
+                generate_page(f"{source}/{f}", template, f"{dest}/{f[:-3]}.html", basepath)
+
+        else:
+            if f not in os.listdir(dest):
+                os.mkdir(f"{dest}/{f}")
+            generate_pages_recursive(source+f"/{f}", template,  dest+f"/{f}", basepath)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+import sys
+from generate.generate_page import generate_pages_recursive
 from generate.copy_directory import copy_directory
-from generate.generate_page import generate_pages_in_directory
 
 
 def main():
+    args = sys.argv
+    base_path = "/"
+    if len(args) == 2:
+        base_path = sys.argv[1]+base_path
+    elif len(args)>2:
+        raise ValueError("Too many arguments")
 
-    copy_directory("static", "public")
-    generate_pages_in_directory("content", "public")
+    static_path = "static"
+    docs_path = "docs"
+    copy_directory(static_path, docs_path)
 
+    generate_pages_recursive("content", "template.html",  docs_path, base_path)
 
 main()
