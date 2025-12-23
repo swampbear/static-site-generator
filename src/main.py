26f098303ea80941b828eb154e80aafd64b7c9d6
Jobjorn <672641@stud.hvl.no>
2025-12-23 18:10:46 +0100

    refactor: move generate pages in directory function to generate_page package

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-import os
 from generate.copy_directory import copy_directory
-from generate.generate_page import generate_page
+from generate.generate_page import generate_pages_in_directory
 
 
 def main():
@@ -9,15 +8,4 @@ def main():
     generate_pages_in_directory("content", "public")
 
 
-def generate_pages_in_directory(source, dest):
-    content_dir = os.listdir(source)
-    for f in content_dir:
-        if os.path.isfile(f"{source}/{f}"):
-            if f[-3:] == ".md":
-                generate_page(f"{source}/{f}", "template.html", f"{dest}/{f[:-3]}.html")
-
-        else:
-            if f not in os.listdir(dest):
-                os.mkdir(f"{dest}/{f}")
-            generate_pages_in_directory(source+f"/{f}", dest+f"/{f}")
 main()
