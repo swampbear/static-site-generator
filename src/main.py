be34c7fd67a3251569d9b60e3c165c2f8fb72a69
Jobjorn <672641@stud.hvl.no>
2025-12-23 23:22:47 +0100

    only repo name

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ from generate.copy_directory import copy_directory
 
 def main():
     args = sys.argv
-    base_path = ""
+    base_path = "/"
     if len(args) == 2:
-        base_path = sys.argv[1]+base_path
+        base_path = sys.argv[1]+"/"
     elif len(args)>2:
         raise ValueError("Too many arguments")
 
